5c16924dbd5f4a4c3b6f23c575c047627b45d36c
jeffmanhart <jeffreymanhart@gmail.com>
2023-09-27 12:02:54 -0600

    made an algorithm for palindrome discovery

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
+const { consoleLog } = require("mocha/lib/reporters/base");
+
 function isPalindrome(word) {
   // Write your algorithm here
+  wordArray = word.split('')
+  let count = wordArray.length -1
+  for (let i = 0; i < wordArray.length; i++) {
+    const forwardLetter = wordArray[i];
+    const backwardLetter = wordArray[count];
+    count--
+    if(forwardLetter===backwardLetter){
+      return true
+    }else{
+      return false
+    }
+  }
+    
 }
+
 
 /* 
   Add your pseudocode here
+  take in string
+  splitletters into array
+  loop through the last letter in the array to compare to the front letters
+  return true or false
 */
 
 /*
   Add written explanation of your solution here
+  isPalindrome() takes in a a single string
+  isPalindrome() then splits the word into an array of characters 
+  defined another counter to the length of the array
+  loop through the word array incrementing the index up every loop
+  also decrement the secondary counter assigned to the array length
+  if both letters are equal then return true
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +46,18 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("moom"));
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("!wow!"));
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("foobar"));
 }
 
 module.exports = isPalindrome;
